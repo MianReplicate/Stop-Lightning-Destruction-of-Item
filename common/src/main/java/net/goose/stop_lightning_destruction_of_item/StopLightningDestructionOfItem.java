82828191f4dec9e3f7ec4520c2b10bd14c5fdf20
LoxiGoose <docgoosenn@gmail.com>
2024-08-31 20:37:19 -0700

    edited logger

--- common/src/main/java/net/goose/stop_lightning_destruction_of_item/StopLightningDestructionOfItem.java
+++ common/src/main/java/net/goose/stop_lightning_destruction_of_item/StopLightningDestructionOfItem.java
@@ -1,11 +1,11 @@
 package net.goose.stop_lightning_destruction_of_item;
 
-import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class StopLightningDestructionOfItem {
     public static final String MOD_ID = "stop_lightning_destruction_of_item";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
         LOGGER.info("FUCK LIGHTNING DESTRUCTION ITEM!!! YEAHHHHHHHHHHHH");
